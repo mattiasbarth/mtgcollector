fb06219806dda9f946e5aa1f6a584a90f043e332
Mattias Barth <mattias.barth@gmail.com>
2020-10-30 21:20:55 +0100

    Implement function to add one or more cards to a collection
    First we try to create a card template by using the name provided by the user. If a card with that name exists in the Multiverse we continue and ask for additional data. When all data in the template is valid it can be transferred to a card object by passing the template dictionary. Initially I thought of parsing the template object but realised it would be unnecessarily complicated when retrieving and creating a card from a collection.

--- main.py
+++ main.py
@@ -24,7 +24,55 @@ def main():
 
 def add_card():
     """Add one or more cards to collection."""
-    pass
+    while True:
+        # Prompt user for a card name as long at card name is invalid.
+        # Query name can be inexact and is case insensitive but may only
+        #   be interpreted  as exactly one card, it can't be ambiguous.
+        query_name = input('Name a card: ')
+        print(f'Looking for {query_name} in the Multiverse...')
+        try:
+            card_template = CardTemplate(query_name)
+        except ValueError as e:
+            print(e, 'Please try again.')
+            continue
+        break
+        # Query name is valid and a card has been found.
+    print(f'Found card {card_template.name}!')
+    count = input(
+        f'How many {card_template.name} do you want to add (1 is default) ')
+    count = count.strip() or 1  # Todo: Check if numeric
+    for _ in range(int(count)):
+        # Prompt user for card expansion, it can't be empty.
+        while True:
+            # Use previous expansion as default when adding
+            #   additional cards.
+            default_message = (f'({card_template.expansion.title()} is default)'
+                               if card_template.expansion else '')
+            expansion = input(f'Expansion{default_message}: ').strip()
+            # Todo: Check expansion validity.
+            card_template.expansion = expansion or card_template.expansion
+            if card_template.expansion:
+                break
+        # User has entered "valid" expansion.
+        # Ask user for specific card data.
+        language = input('Language (en is default): ')
+        condition = input('Condition (ex is default): ')
+        is_foil = input('Foil (y/n, n is default): ')
+        is_signed = input('Signed (y/n, n is default): ')
+        is_alt_art = input('Alt art (y/n, n is default): ')
+        notes = input('Notes: ')
+        card_template.set_specific_data(
+            language,
+            condition,
+            is_foil,
+            is_signed,
+            is_alt_art,
+            notes,
+        )
+        card = Card(card_template.__dict__)
+        card.add_to_collection(COLLECTION)
+        print(f'Card {card} added to collection {COLLECTION.name}!')
+        print()
 
 
 def browse_collection():
